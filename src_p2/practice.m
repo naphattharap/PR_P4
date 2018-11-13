c8ff99f003b8f15a911ee543202cce5f685146c2
Naphatthara <>
2018-11-13 22:16:46 +0100

    changed default sorting to kernel function and accuracy score

--- src_p2/practice.m
+++ src_p2/practice.m
@@ -262,7 +262,7 @@ end
 T = table(colKernelFunction, colPolynomilDegree, colPca, colLda, ...
     colCoding, colStandardize, colAccuracy);
 
-B = sortrows(T, [1, 2]);
+B = sortrows(T, [1, 7]);
 
 
 
